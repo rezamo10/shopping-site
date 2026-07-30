0523c66c8101e6787fa4baab53d67ca5d6b6fbde
Reza Moradi <rezamo2121@gmail.com>
2026-07-30 20:12:47 +0330

    update header and mobile menu added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 const navLinks = document.querySelectorAll(".nav-link");
-
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const menu = document.querySelector(".menu");
+console.log(menuBtn)
 navLinks.forEach(link => {
 	link.addEventListener("click" , (e)=>{
 		navLinks.forEach(l => l.classList.remove("active"));
 		e.target.classList.add("active");
 	})
 })
+function hideMenu() {
+	menu.classList.remove("active");
+}
+function showMenu() {
+	menu.classList.add("active");
+}
+menuBtn.addEventListener("click", showMenu);
+closeBtn.addEventListener("click", hideMenu);
