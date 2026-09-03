273e9892b13930bae6ceac0a8589eb20db84f40b
Reza Moradi <rezamo2121@gmail.com>
2026-09-03 13:05:04 +0330

    update: mobile menu resposive & submenus

--- src/main.js
+++ src/main.js
@@ -7,21 +7,55 @@ import 'swiper/css/pagination';
 
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
-const menu = document.querySelector(".menu");
+const menuWrapper = document.querySelector(".menu-wrapper");
+const overlay = document.querySelector(".overlay");
 const mailInput = document.querySelector("#mail-input");
 const popMessage = document.querySelector(".pop-message");
 const mailBtnMobile = document.querySelectorAll(".mail-btn-data");
 const footerItem = document.querySelectorAll(".footer-item");
 const categoryListBtn = document.querySelectorAll(".category-list-btn");
 const panelContainers = document.querySelectorAll(".panel");
+const menuItem = document.querySelectorAll(".menu-item");
+const menu = document.querySelectorAll(".menu");
+const menuBackButton = document.querySelectorAll(".menu-back-button");
 let toastTimer;
 let dataCategory;
 
+menuItem.forEach((item) => {
+	item.addEventListener("click", (e) => {
+		// console.log(e.target.dataset.menuId)
+		menu.forEach((menu) => {
+			if (menu.dataset.menuId === "1") {
+				menu.classList.remove("menu-translateX-left");
+				menu.classList.add("menu-translateX");
+			}
+			if (e.target.dataset.menuId === menu.dataset.menuId) {
+				menu.classList.remove("menu-translateX-right");
+				menu.classList.add("menu-translateX-left");
+			}
+		});
+	});
+});
+menuBackButton.forEach((item) => {
+	item.addEventListener("click", () => {
+		// item.preventDefault();
+		menu.forEach((menu) => {
+			menu.classList.remove("menu-translateX-left","menu-translateX");
+			menu.classList.add("menu-translateX-right");
+			if (menu.dataset.menuId === "1") {
+				menu.classList.remove("menu-translateX");
+				menu.classList.add("menu-translateX-left");
+			}
+		});
+	});
+});
 function hideMenu() {
-	menu.classList.remove("active");
+	menuWrapper.classList.remove("active");
+	overlay.classList.remove("active");
 }
 function showMenu() {
-	menu.classList.add("active");
+	menuWrapper.classList.add("active");
+	overlay.classList.add("active");
 }
 function checkEmail() {
 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -178,3 +212,4 @@ menuBtn.addEventListener("click", showMenu);
 closeBtn.addEventListener("click", hideMenu);
 mailInput.addEventListener("input", toggleBtn);
 mailBtnMobile.forEach(btn => btn.addEventListener("click", checkEmail));
+overlay.addEventListener("click", hideMenu);
