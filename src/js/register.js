20501cea53fcdbcc4229aab91d176ffa86001bff
Reza Moradi <rezamo2121@gmail.com>
2026-09-04 16:06:11 +0330

    connect login and register to database

--- src/js/register.js
+++ src/js/register.js
@@ -1,3 +1,6 @@
+import {supabase} from './supabaseClient.js';
+import {showAlert} from './utils/sweetalert.js';
+
 const emailInput = document.querySelector('#email');
 const repeatPasswordInput = document.querySelector('#repeat-password');
 const passwordInput = document.querySelector('#password');
@@ -5,12 +8,12 @@ const eyeIcon = document.querySelectorAll('.eye-icon');
 const eyeOffIcon = document.querySelectorAll('.eye-off-icon');
 const showPasswordBtn = document.querySelectorAll('.show-password-btn');
 const strengthBar = document.querySelector("#strength-bar");
+const sendBtn = document.querySelector("#send-btn");
 let debounceTimer;
 
 function checkEmail(email) {
 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	const checkEmail = emailRegex.test(email);
-	console.log(email);
 	emailInput.classList.remove("border-neutral-200", "border-green-500", "border-red-500");
 	if (email === "") {
 		emailInput.classList.add("border-neutral-200");
@@ -19,6 +22,7 @@ function checkEmail(email) {
 	} else {
 		emailInput.classList.add("border-red-500");
 	}
+	if (checkEmail) return true;
 }
 
 const levels = [
@@ -59,7 +63,6 @@ function showPassword(password, repeat) {
 	const isPassword = passwordInput.type === 'password' && repeat.type === 'password';
 	password.type = isPassword ? 'text' : 'password';
 	repeat.type = isPassword ? 'text' : 'password';
-	console.log(passwordInput);
 	eyeIcon.forEach(icon => icon.classList.toggle('hidden', !isPassword));
 	eyeOffIcon.forEach(icon => icon.classList.toggle('hidden', isPassword));
 }
@@ -74,6 +77,48 @@ function checkPassword(password, repeated) {
 	}
 }
 
+async function sendData() {
+	const email = emailInput.value.trim();
+	const password = passwordInput.value;
+	const repeated = repeatPasswordInput.value;
+	const isEmailMethodTrue = checkEmail(email);
+
+	if (!isEmailMethodTrue) {
+		showAlert(
+			"Please enter a valid email address",
+			"error",
+			"OK"
+		);
+		return;
+	}
+	if (password !== repeated) {
+		showAlert(
+			"Passwords do not match",
+			"error",
+			"OK"
+		);
+		return;
+	}
+	if (password.trim().length === 0 || repeated.trim().length === 0){
+		showAlert(
+			"Please enter your password",
+			"error",
+			"OK"
+		);
+		return;
+	}
+	const {data , error} = await supabase.auth.signUp({email,password});
+	if (error) {
+		showAlert(error.message, "error", "OK");
+		return;
+	}
+	showAlert(
+		"Your account has been created successfully!",
+		"success",
+		"OK"
+	).then(data => window.location.href = "login.html")
+}
+
 showPasswordBtn.forEach(btn => btn.addEventListener('click', () => showPassword(passwordInput, repeatPasswordInput)));
 passwordInput.addEventListener("input", (e) => {
 	updatePasswordUI(e.target.value);
@@ -81,10 +126,10 @@ passwordInput.addEventListener("input", (e) => {
 emailInput.addEventListener("input", (e) => {
 	checkEmail(e.target.value);
 });
-
 repeatPasswordInput.addEventListener('input', () => {
 	clearTimeout(debounceTimer);
 	debounceTimer = setTimeout(() => {
 		checkPassword(passwordInput.value, repeatPasswordInput.value);
 	}, 500);
 });
+sendBtn.addEventListener('click', sendData);
